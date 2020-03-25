c68167c0a991914ba9c19096c737d0cf934fd0fb
Māris Lagzdiņš (TestDevLab) <maaris.lagzdins+developer@gmail.com>
2020-03-25 18:56:13 +0200

    Finished Project 5 challange

---
+++ Project 5/Challange/WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,134 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Maris Lagzdins on 20/03/2020.
+//  Copyright © 2020 Developer Maris. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    @State private var score = 0
+
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocapitalization(.none)
+                    .padding()
+
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                }
+                Text("Score: \(score)")
+            }
+            .navigationBarTitle(rootWord)
+            .navigationBarItems(leading: Button(action: startGame) {
+                Text("Start new game")
+            })
+            .onAppear(perform: startGame)
+            .alert(isPresented: $showingError) {
+                Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
+            }
+        }
+    }
+
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+
+        guard answer.count >= 3 else {
+            wordError(title: "Incorrect word", message: "Word must contain at least 3 characters")
+            return
+        }
+
+        guard answer != rootWord.lowercased() else {
+            wordError(title: "Incorrect word", message: "You cannot use the same word")
+            return
+        }
+
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+
+        guard isReal(word: answer) else {
+            wordError(title: "Word not possible", message: "That isn't a real word")
+            return
+        }
+
+        usedWords.insert(answer, at: 0)
+        score += 1 + answer.count
+        newWord = .empty
+    }
+
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: .newlines)
+                rootWord = allWords.randomElement() ?? "silkworm"
+                usedWords = [String]()
+                score = 0
+                return
+            }
+        }
+
+        fatalError("Could not load start.txt from bundle.")
+    }
+
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord.lowercased()
+
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+
+        return true
+    }
+
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
+        return misspelledRange.location == NSNotFound
+    }
+
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+extension String {
+    static let empty = ""
+}
