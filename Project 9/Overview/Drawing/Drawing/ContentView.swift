907181641d5dbc58eea3ec5f34aa1d7bdae1da44
Māris Lagzdiņš (TestDevLab) <maaris.lagzdins+developer@gmail.com>
2020-05-02 14:32:12 +0300

    Finished Project 9 1st part

---
+++ Project 9/Overview/Drawing/Drawing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Maris Lagzdins on 02/05/2020.
+//  Copyright © 2020 Developer Maris. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
